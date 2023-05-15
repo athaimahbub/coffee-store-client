d23c2029806c3390b50a0e40503751e639a7504e
athaimahbub <athaimahbub2016@gmail.com>
2023-05-15 20:54:34 +0600

    first commit

---
+++ src/component/AddCoffee.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars,react/no-unescaped-entities,no-undef*/
+
+
+import React from 'react';
+
+const AddCoffee = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/component/UpdateCoffee.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars,react/no-unescaped-entities,no-undef,react/jsx-no-comment-textnodes */
+
+import React from 'react';
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UpdateCoffee;
